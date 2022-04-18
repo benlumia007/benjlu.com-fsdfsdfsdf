458314dbcfb6f59608e9100b00afa6f48287c6ff
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-04-18 11:30:53 -0700

    initial commit

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Main configuration file
+ *
+ * @package   benjlu.com
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright Copyright (C) 2014-2022. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/benlumia007/benjlu.com
+ */
+
+return [
+	'build' => [
+		'source' => 'public'
+	]
+];
---
+++ helpers.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Helpers file
+ *
+ * @package   benjlu.com
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright Copyright (C) 2014-2022. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/benlumia007/benjlu.com
+ */
+
+return [
+
+];
